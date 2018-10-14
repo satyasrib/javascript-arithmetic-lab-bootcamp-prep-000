079368170e6695665752d78314e796d456846038
Satyasri Varaganti <satyasrib@gmail.com>
2018-10-14 23:46:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
-var a = 1
-var b = 80
+
 function add(a, b) {
   return a + b
 }
-var a = 60
-var b = 40
 function subtract(a, b) {
   return a - b
 }
-var a = 2
-var b = 3.4
 function multiply(a, b) {
   return a * b
 }
-var a = 5.0
-var b = 2.5
 function divide(a, b) {
   return a / b
 }
-var n
 function increment(n) {
   return n++
 }
