edd2b85f849813a9910011280da24a028cdbe6bc
Satyasri Varaganti <satyasrib@gmail.com>
2018-10-15 01:14:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function inc(n) {
 function dec(n) {
   return --n
 }
-function makeInt(n , parseInt(n)) {
-  return n
+function makeInt(n) {
+  return parseInt(n, 10)
 }
 function preserveDecimal(n) {
   return n
